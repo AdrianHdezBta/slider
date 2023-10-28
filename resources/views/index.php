486ae456c407d3bbabc5ca4b5af447b4ff1bf76e
AdrianHdezBta <btaadrian@gmail.com>
2023-10-27 20:36:43 -0600

    primer lanzamiento

---
+++ resources/views/index.php
@@ -0,0 +1,68 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Imagenes deportivos</title>
+    <link rel="stylesheet" href="assets/style.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css">
+</head>
+
+<body>
+    <div class="container">
+        <div id="slide">
+            <div class="item" style="background-image: url(https://media.gq.com.mx/photos/5f5a9a4cef6115112e88c6d7/16:9/w_2560%2Cc_limit/BMW%2520M%2520NEXT2.jpg);">
+                <div class="content">
+                    <div class="name">Carros deportivos</div>
+                    <div class="des">Visita las imágenes perfectas de auto deportivo.</div>
+                    <button>Ver más</button>
+                </div>
+            </div>
+            <div class="item" style="background-image: url(https://i.pinimg.com/originals/c5/f9/56/c5f956cd20876e62210debe869270da1.jpg);">
+                <div class="content">
+                    <div class="name">Carros deportivos</div>
+                    <div class="des">Visita las imágenes perfectas de auto deportivo.</div>
+                    <button>Ver más</button>
+                </div>
+            </div>
+            <div class="item" style="background-image: url(https://www.elcarrocolombiano.com/wp-content/uploads/2019/06/20190625-BMW-VISION-M-NEXT-CONCEPT-CAR-01.jpg);">
+                <div class="content">
+                    <div class="name">Carros deportivos</div>
+                    <div class="des">Visita las imágenes perfectas de auto deportivo.</div>
+                    <button>Ver más</button>
+                </div>
+            </div>
+            <div class="item" style="background-image: url(https://hips.hearstapps.com/hmg-prod/images/presentacion-lamborghini-2022-madrid-148-1668103001.jpg);">
+                <div class="content">
+                    <div class="name">Carros deportivos</div>
+                    <div class="des">Visita las imágenes perfectas de auto deportivo.</div>
+                    <button>Ver más</button>
+                </div>
+            </div>
+            <div class="item" style="background-image: url(https://img.freepik.com/fotos-premium/futuristic-car-with-logo-of-the-company-that-says-the-car-of-the-future_908846-2622.jpg);">
+                <div class="content">
+                    <div class="name">Carros deportivos</div>
+                    <div class="des">Visita las imágenes perfectas de auto deportivo.</div>
+                    <button>Ver más</button>
+                </div>
+            </div>
+            <div class="item" style="background-image: url(https://img.freepik.com/fotos-premium/lamborghini-carretera-cielo-nublado-fondo_148840-1160.jpg);">
+                <div class="content">
+                    <div class="name">Carros deportivos</div>
+                    <div class="des">Visita las imágenes perfectas de auto deportivo.</div>
+                    <button>Ver más</button>
+                </div>
+            </div>
+        </div>
+        <div class="buttons">
+            <button id="prev"><i class="fa-solid fa-angle-left"></i></button>
+            <button id="next"><i class="fa-solid fa-angle-right"></i></button>
+        </div>
+    </div>
+
+    <script src="assets/script.js"></script>
+</body>
+
+</html>
